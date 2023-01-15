df1a57ea87d087e76da107d0b746388c8310c81a
kay3000 <kindnesskay1@gmail.com>
2023-01-15 15:16:26 +0100

    fixed rating  component files

--- Rating_component/main.js
+++ Rating_component/main.js
@@ -3,28 +3,25 @@ let scores = document.querySelectorAll(".score");
 const thanks = document.querySelector("#thanks");
 const rate = document.querySelector("#rate");
 let submit = document.querySelector("#sub");
-let num=document.querySelector('#no_rating')
+let num = document.querySelector("#no_rating");
 
 scores.forEach((element) => {
   element.addEventListener("click", score);
 });
 function score() {
   let index = this.id;
-  ratings = index;
-  console.log(ratings);
-  if (document.querySelector(".scoreActive")) {
-    scores.forEach((element) => {
-      element.classList.remove("scoreActive");
+  if(document.getElementsByClassName('score_active')){
+    scores.forEach(element => {
+      element.className='score';
     });
   }
-  for (let i = 0; i < index; i++) {
-    scores[i].classList.add("scoreActive");
-  }
+  this.className='score_active';
+  ratings = index;
 }
 
 submit.addEventListener("click", next);
 function next() {
-rate.classList.add('hide');
-thanks.classList.remove('hide');
-num.innerHTML=`you selected ${ratings} out of 5`;
+  rate.classList.add("hide");
+  thanks.classList.remove("hide");
+  num.innerHTML = `you selected ${ratings} out of 5`;
 }
